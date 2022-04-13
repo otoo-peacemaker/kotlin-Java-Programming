41ec84936209faaef1e11c3cda1d7bb23c7af888
Peacemaker <peacemaker.otoo@amalitech.org>
2022-04-13 22:17:34 +0000

    finished with register user implementation

--- ktor/ktor-sample/src/main/kotlin/com/peacemaker/repository/UserRepositoryImpl.kt
+++ ktor/ktor-sample/src/main/kotlin/com/peacemaker/repository/UserRepositoryImpl.kt
@@ -2,14 +2,33 @@ package com.peacemaker.repository
 
 import com.peacemaker.BaseResponse.BaseResponse
 import com.peacemaker.service.RegisterUser
+import com.peacemaker.service.UserService
 
-class UserRepositoryImpl : UserRepository {
-    /**For Repository to make web request*/
+class UserRepositoryImpl(private val userService: UserService) : UserRepository {
+
+    /**Check user by email, if exist throw[exception] else register new user
+     * */
     override suspend fun registerUser(params: RegisterUser): BaseResponse<Any>? {
-        TODO("Not yet implemented")
+
+        return if(isEmailExist(params.email)){
+            BaseResponse.ErrorResponse(message = "Email already Exist")
+        }else{
+            val user  = userService.registerUser(params)
+            if (user!=null){
+                //Todo generate auth token
+                BaseResponse.SuccessResponse(data = user)
+            }else{
+                BaseResponse.ErrorResponse()
+            }
+        }
     }
 
     override suspend fun loginUser(email: String, password: String): BaseResponse<Any>? {
         TODO("Not yet implemented")
+    }
+
+    /**checking if email exist or not*/
+    private suspend fun isEmailExist(email: String):Boolean {
+        return userService.findUserByEmail(email) !=null
     }
 }

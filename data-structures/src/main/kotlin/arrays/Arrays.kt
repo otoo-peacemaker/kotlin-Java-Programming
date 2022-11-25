ec77b1d34b5b0dd2fe8e84009d5934246920b60e
Peacemaker Otoo <infofeedacc@gmail.com>
2022-11-25 18:38:07 +0000

    modified

--- data-structures/src/main/kotlin/arrays/Arrays.kt
+++ data-structures/src/main/kotlin/arrays/Arrays.kt
@@ -8,13 +8,13 @@ class ArraysClass {
 
     fun reverseArrayInGroup(arr:ArrayList<Int>, groupSize:Int){
         val length = arr.size
-        var groupPos=0
 
         for(i in 0..length){
-            groupPos = i+groupSize//0+groupSize
 
             var start = i
-            var end = Math.min(groupPos-1, length-1)//solving out of bound exception and finding min arr size and group size
+            val  groupPos = i+groupSize//0+groupSize
+            var end =
+                (groupPos - 1).coerceAtMost(length - 1)//solving out of bound exception and finding min arr size and group size
 
             while (start<=end){
                 val temp = arr[start]
@@ -29,8 +29,8 @@ class ArraysClass {
 //webwrite.com
 }
 fun main(){
-    val arr = arrayListOf<Int>(3,6,7,8,1,28,9,53,7,4,2,9,2,9,0)
-    val k = 5
+    val arr = arrayListOf<Int>(3,6,7,8, 1,9,9,3 ,7,4,2,9, 2,9,0)
+    val k = 3
     val reverseArrayInGroup = ArraysClass()
     reverseArrayInGroup.reverseArrayInGroup(arr, k)
 
